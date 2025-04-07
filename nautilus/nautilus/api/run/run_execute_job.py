3c0d6e0c67e51c809d5f011e2a9f2962b39a479c
seolzero <sulyoungpk@gmail.com>
2025-04-07 16:22:19 +0900

    update param namespace

--- nautilus/nautilus/api/run/run_execute_job.py
+++ nautilus/nautilus/api/run/run_execute_job.py
@@ -16,8 +16,8 @@ from nautilus.core.communicate.k8s import (
     get_pod_name_by_deployment
 )
 
-def run_nvflare_job_in_pod(pod_name: str, project_id: str, job_id: str):
-    cmd = f"kubectl exec -i {pod_name} -- /workspace/nautilus/nautilus/workspace/provision/{project_id}/prod_00/admin@nvidia.com/startup/fl_admin.sh"
+def run_nvflare_job_in_pod(pod_name: str, project_id: str, job_id: str, namespace: str = "nautilus"):
+    cmd = f"kubectl exec -i {pod_name} -n {namespace} -- /workspace/nautilus/nautilus/workspace/provision/{project_id}/prod_00/admin@nvidia.com/startup/fl_admin.sh"
     print(f"- Executing (cmd): {cmd}")
     child = pexpect.spawn(cmd, encoding='utf-8')
 
@@ -30,7 +30,8 @@ def run_nvflare_job_in_pod(pod_name: str, project_id: str, job_id: str):
     child.expect("Done")
     print(child.before)
 
-def main(project_id, job_id):
+
+def main(project_id, job_id, namespace):
     pod_name = f"{project_id}-server"
     print(f"Starting execute job for Project: {project_id}")
     '''
@@ -38,15 +39,17 @@ def main(project_id, job_id):
     server_pod_full_name = get_pod_name_by_deployment(deployment_name=server_pod_name)
     print(f"Deploying Server | Pod: {server_pod_name} | pod_full_name: {server_pod_full_name}")    
     '''
-    run_nvflare_job_in_pod(pod_name="mylocalhost", project_id=project_id, job_id=job_id)
-    
+    run_nvflare_job_in_pod(pod_name="mylocalhost", project_id=project_id, job_id=job_id, namespace=namespace)
+        
     print("Job execution completed successfully!")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Deploy Nautilus Federated Learning Simulation")
-    parser.add_argument("--project_id", type=str, required=True, help="project_id")
-    parser.add_argument("--job_id", type=str, required=True, help="Job name")
+    parser.add_argument("--project_id", type=str, required=True, help="Project ID")
+    parser.add_argument("--job_id", type=str, required=True, help="Job ID")
+    parser.add_argument("--namespace", type=str, default="nautilus", help="Kubernetes namespace (default: nautilus)")
 
     args = parser.parse_args()
 
-    main(args.project_id, args.job_id)
+    main(args.project_id, args.job_id, args.namespace)
